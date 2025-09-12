7223c243d4389ae8dec7c0637163dc51b16068d0
Johncakes <johnnygamesover9000@gmail.com>
2025-09-12 10:50:24 +0900

    Feat : view 와 baseBallGame 연결 구현

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import BaseBallGame from "./baseBallGame.js";
+import View from "./view.js";
+
 const baseBallGame = BaseBallGame();
+const view = View({
+    onSubmit: (userInput) => {
+        try {
+            const result = baseBallGame.userInput(userInput);
+            if (result === "win"){
+                view.gameWinView();
+            }
+            else{
+                view.updateView(result);
+            }
+        } catch (error) {
+            view.errorView();
+        }
+        
+    },
+    onRestart: () => {
+        baseBallGame.startGame();
+        view.gameStartView();
+    }
+});
+
+view.gameStartView();
 baseBallGame.startGame();
